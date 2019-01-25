80503eee15a36055b6c878f47a195e7c899612e7
iBurntWaffle <marjoe.velasco1@gmail.com>
2019-01-25 21:03:56 +0800

    animation added, unused button variables removed

--- app/src/main/java/com/example/poy/capstonedraftv8/MainActivity.java
+++ app/src/main/java/com/example/poy/capstonedraftv8/MainActivity.java
@@ -47,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> myList;
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
