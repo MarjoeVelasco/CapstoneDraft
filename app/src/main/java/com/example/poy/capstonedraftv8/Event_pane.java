df24c871a88d20e3240cfc8c0fc71aa29173803c
iBurntWaffle <marjoe.velasco1@gmail.com>
2019-01-26 16:04:40 +0800

    query for editing and deleting events added

--- app/src/main/java/com/example/poy/capstonedraftv8/Event_pane.java
+++ app/src/main/java/com/example/poy/capstonedraftv8/Event_pane.java
@@ -1,27 +1,42 @@
 package com.example.poy.capstonedraftv8;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Event_pane extends AppCompatActivity {
 
-    TextView tryid;
+    myDbAdapter helper;
+    TextView pane,pane2,r1,time,desc;
+    TextView btn;
+    Button save,cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_pane);
 
-        tryid = (TextView)findViewById(R.id.tryid);
+        helper = new myDbAdapter(this);
+        save=(Button)findViewById(R.id.save);
+        cancel=(Button)findViewById(R.id.cancel);
+        pane = (TextView) findViewById(R.id.textView);
+        pane2 = (TextView) findViewById(R.id.textView7);
+        time = (TextView) findViewById(R.id.time);
+        r1 = (TextView) findViewById(R.id.r1);
+        desc = (TextView) findViewById(R.id.desc);
 
         Intent intent = getIntent();
 //Get the USERNAME passed from IntentExampleActivity
         String event_id = (String) intent.getSerializableExtra("event_id");
         String date_id = (String)intent.getSerializableExtra("date_id");
 
-        tryid.setText(event_id+" "+date_id);
+        Cursor dbres = helper.getStartDate(date_id);
+
+
+
 
     }
 }
