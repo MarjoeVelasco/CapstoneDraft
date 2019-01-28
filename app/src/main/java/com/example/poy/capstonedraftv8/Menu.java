d352ae1f7cfe36320a214095acb49fff2dddcdb2
iBurntWaffle <marjoe.velasco1@gmail.com>
2019-01-29 07:33:16 +0800

    menu added (partially done)

--- app/src/main/java/com/example/poy/capstonedraftv8/Menu.java
+++ app/src/main/java/com/example/poy/capstonedraftv8/Menu.java
@@ -2,12 +2,16 @@ package com.example.poy.capstonedraftv8;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
+
+    Button btnPrefs,btnVerse,btnPrefs2,btnVerse2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
     }
 }
--- app/src/main/java/com/example/poy/capstonedraftv8/SplashActivity.java
+++ app/src/main/java/com/example/poy/capstonedraftv8/SplashActivity.java
@@ -16,7 +16,7 @@ import android.widget.ImageView;
 public class SplashActivity extends AppCompatActivity {
 
     private ImageView logo;
-    private static int splashTimeOut=5000;
+    private static int splashTimeOut=4000;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
